416748d05a6fcb236704afd7d9111aa9ca570a7e
Andrew Spano <andrew.spano1@marist.edu>
2012-10-24 00:36:23 -0400

    Added valid commands var to appear whenever an invalid command is entered. Did not add it to code logically yet.

--- script.js
+++ script.js
@@ -28,6 +28,14 @@ var score = 0;
 var bool_kitchen = false;
 var bool_presentation = false;
 var bool_corridor = false;
+//important var which will appear whenever the user inputs an invalid command.
+var validCommands = "\nValid Directions are: (North, South, East and West\n"
+		    + "Which moves the player one unit in that direction\n"
+		    + "Ex: North increments y position by one,"
+		    + " West decrements x positon by one."
+		    + "\nShorthand commands n,s,w,e correspond to North, South"
+		    + " East and West respectively."
+		    + " The input parser is case insensitive.";
 
 
 function but1() {
