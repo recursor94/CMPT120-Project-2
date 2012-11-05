7a9d643b60ffc0687b535ab5ed00fdfb353aa596
Andrew Spano <andrew.spano1@marist.edu>
2012-11-05 00:21:08 -0500

    Added placeholder functions for all ten locations currently existent in the game.

--- locations.js
+++ locations.js
@@ -13,5 +13,47 @@ function boundary() {
      * button disabling action. In the interest of concision and clarity,
      * it is probably best to put all boundary code in one function.
      */
-    alert("player has reached a boundary");
+    alert("You have reached a boundary");
+}
+
+function suite () {
+    alert("You have entered your suite");
+}
+
+function kitchen() {
+    alert("You have entered the Kitchen.");
+}
+
+function presentationRoom() {
+    alert("You have entered the Presentation room");
+}
+
+function bar(){
+    alert("You have entered the space bar");
+}
+
+function closet() {
+    alert("You have entered the closet");
+}
+
+function restroom() {
+    alert("You have entered the restroom");
+}
+
+/*The proceeding functions are for the three new locations, they
+*will follow the same format as the other locations so far,
+*one above and bellow the corridor. Eg. (xpos,0), (xpos,2)*/
+
+function armory() {
+    //The player will find his first weapon--the space laser gun here
+    alert("You have entered the armory.");
+}
+
+function hospitalRoom() {
+    alert("You have entered the hospital room.");
+}
+
+function freightDeck() {
+    alert("You have entered the freight deck.");
+    //Note:possibility of a discarded empty freight container location?
 }
