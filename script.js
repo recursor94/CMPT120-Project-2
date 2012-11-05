e615f5bab7f9418e090ae8e71dd3a81e97dea567
Andrew Spano <andrew.spano1@marist.edu>
2012-11-04 23:55:50 -0500

    added locations.js to be loaded before script.js in index.html. this will allow Location functions to be called from game logic script.js file.

--- script.js
+++ script.js
@@ -381,21 +381,3 @@ function enableAllButtons() {
     bool_canEast = true;
     bool_canWest = true;
 }
-
-//LOCATION EVENT HANDLERS START HERE
-
-
-function corridor() {
-    //temporary place holder for proof of success, will update later with
-    //location specific logic as in testLocation
-    alert ("You are now in the corridor");
-}
-
-function boundary() {
-    /*Should be default switch case; this is the case for boundaries.
-     * will likely test for right bound or left bound and take the appropriate
-     * button disabling action. In the interest of concision and clarity,
-     * it is probably best to put all boundary code in one function.
-     */
-    alert("player has reached a boundary");
-}
