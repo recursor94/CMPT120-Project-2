df887f52e2a01a9c42e4c6e5f24dcf254c008b65
Andrew Spano <andrew.spano1@marist.edu>
2012-12-08 21:09:21 -0500

    fixed changes to currentLocation to avoid new var declaration each time created officePuzzleTest definition and added puzzle test to locations

--- puzzle.js
+++ puzzle.js
@@ -1,3 +1,8 @@
 function officePuzzleTest() {
-    
+    if(hasItem("mysterious blueprints")) {
+        updateText("There is a decoder here--the serial number on the label"
+                   + "matches the serial number on your blueprints");
+        }
+        
+
 }
