26ff8e6c11d0902d3d36d48b79988a3878c4806c
Andrew Spano <andrew.spano1@marist.edu>
2012-12-08 21:57:25 -0500

    added extra conditional to officepuzzletest to officepuzzletest function

--- puzzle.js
+++ puzzle.js
@@ -1,5 +1,5 @@
 function officePuzzleTest() {
-    if(hasItem("mysterious blueprints")) {
+    if(playerHasItem("mysterious blue prints") && puzzleCompleted === false) {
         updateText("There is a decoder here--the serial number on the label"
                    + "matches the serial number on your blueprints");
         }
